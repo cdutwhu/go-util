2fe7b348f4a2c00a3758b7fa3b3ac9536fe3526e
Qing Miao <cdutwhu@outlook.com>
2019-07-11 12:24:25 +1000

    + ConditionAssign

--- oper.go
+++ oper.go
@@ -8,8 +8,8 @@ func IF(condition bool, block1, block2 interface{}) interface{} {
 	return block2
 }
 
-// CaseAssign : NO S/C, MUST all valid, e.g. type assert, nil pointer, out of index
-func CaseAssign(checkCasesValues ...interface{}) interface{} {
+// MatchAssign : NO ShortCut, MUST all valid, e.g. type assert, nil pointer, out of index
+func MatchAssign(checkCasesValues ...interface{}) interface{} {
 	l := len(checkCasesValues)
 	PC(l < 3 || l%2 == 0, fEf("Invalid parameters"))
 	_, l1, l2 := 1, (l-1)/2, (l-1)/2
@@ -23,4 +23,27 @@ func CaseAssign(checkCasesValues ...interface{}) interface{} {
 	}
 	PC(true, fEf("Invalid parameters"))
 	return nil
+}
+
+// ConditionAssign : NO ShortCut, MUST all valid, e.g. type assert, nil pointer, out of index
+func ConditionAssign(condsValuesDft ...interface{}) interface{} {
+	l := len(condsValuesDft)
+	PC(l < 3 || l%2 == 0, fEf("Invalid parameters"))
+
+	l1, l2 := l/2, l/2
+	conds := condsValuesDft[0:l1]
+	values := condsValuesDft[l1 : l1+l2]
+	dft := condsValuesDft[l-1]
+
+	for i, c := range conds {
+		switch c.(type) {
+		case bool:
+			if c.(bool) == true {
+				return values[i]
+			}
+		default:
+			panic(fSf("The <%d> condition param : '%s' MUST be <bool> expression", i+1, c))
+		}
+	}
+	return dft
 }
--- oper_test.go
+++ oper_test.go
@@ -6,6 +6,11 @@ func TestTerOp(t *testing.T) {
 	fPln(IF(1 == 2, "abc", "def").(string))
 }
 
-func TestCaseAssign(t *testing.T) {
-	fPln(CaseAssign("22", "1", "22", 3, 4).(int))
+func TestMatchAssign(t *testing.T) {
+	fPln(MatchAssign("22", "1", "22", 3, 4).(int))
+}
+
+func TestConditionAssign(t *testing.T) {
+	a := 1
+	fPln(ConditionAssign(a+1 == 2, a > 1, a > 0, "a", "b", "c", "dft"))
 }
