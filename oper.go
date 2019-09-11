bd86c722aff361cad2a8a5b681139527e7899087
Qing Miao <cdutwhu@outlook.com>
2019-09-11 10:06:46 +1000

    * MatchAssign

--- oper.go
+++ oper.go
@@ -9,32 +9,29 @@ func IF(condition bool, block1, block2 interface{}) interface{} {
 }
 
 // MatchAssign : NO ShortCut, MUST all valid, e.g. type assert, nil pointer, out of index
-func MatchAssign(checkCasesValues ...interface{}) interface{} {
-	l := len(checkCasesValues)
-	PC(l < 3 || l%2 == 0, fEf("Invalid parameters"))
+func MatchAssign(chkCasesValues ...interface{}) interface{} {
+	l := len(chkCasesValues)
+	PC(l < 4 || l%2 == 1, fEf("Invalid parameters"))
 	_, l1, l2 := 1, (l-1)/2, (l-1)/2
-	check := checkCasesValues[0]
-	cases := checkCasesValues[1 : 1+l1]
-	values := checkCasesValues[1+l1 : 1+l1+l2]
+	check := chkCasesValues[0]
+	cases := chkCasesValues[1 : 1+l1]
+	values := chkCasesValues[1+l1 : 1+l1+l2]
 	for i, c := range cases {
 		if check == c {
 			return values[i]
 		}
 	}
-	PC(true, fEf("Invalid parameters"))
-	return nil
+	return chkCasesValues[l-1]
 }
 
 // TrueAssign : NO ShortCut, MUST all valid, e.g. type assert, nil pointer, out of index
 func TrueAssign(condsValuesDft ...interface{}) interface{} {
 	l := len(condsValuesDft)
 	PC(l < 3 || l%2 == 0, fEf("Invalid parameters"))
-
 	l1, l2 := l/2, l/2
 	conds := condsValuesDft[0:l1]
 	values := condsValuesDft[l1 : l1+l2]
 	dft := condsValuesDft[l-1]
-
 	for i, c := range conds {
 		switch c.(type) {
 		case bool:
--- oper_test.go
+++ oper_test.go
@@ -3,14 +3,17 @@ package util
 import "testing"
 
 func TestTerOp(t *testing.T) {
+	fPln(IF(1 == 1, "abc", "def").(string))
 	fPln(IF(1 == 2, "abc", "def").(string))
 }
 
 func TestMatchAssign(t *testing.T) {
-	fPln(MatchAssign("22", "1", "22", 3, 4).(int))
+	fPln(MatchAssign("22", "1", "22", 3, 4, 5).(int))
+	fPln(MatchAssign("22", "1", "222", 3, 4, 5).(int))
 }
 
-func TestConditionAssign(t *testing.T) {
+func TestTrueAssign(t *testing.T) {
 	a := 1
-	fPln(ConditionAssign(a+1 == 2, a > 1, a > 0, "a", "b", "c", "dft"))
+	fPln(TrueAssign(a+1 == 2, a > 1, a > 0, "a", "b", "c", "dft"))
+	fPln(TrueAssign(a+1 == 20, a > 1, a > 10, "a", "b", "c", "dft"))
 }
